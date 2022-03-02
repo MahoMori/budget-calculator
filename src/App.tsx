62b71c67ac8e31cd25ca6a815fb6e0b29f2d3a04
MahoMori <tr27e1104@gmail.com>
2022-03-02 14:55:53 -0800

    added delete and reset feature

--- src/App.tsx
+++ src/App.tsx
@@ -10,35 +10,45 @@ type Item = {
 // 数字かどうかチェック
 
 function App() {
+  // budget
   const [budget, setBudget] = useState<string>("0");
   const [isChangingBudget, setIsChangingBudget] = useState<boolean>(false);
   const changeBudget = (): void => {
     isChangingBudget ? setIsChangingBudget(false) : setIsChangingBudget(true);
   };
 
+  // reset
+  const handleReset = (): void => {
+    if (window.confirm("Do you want to reset all?")) {
+      setItems([]);
+    }
+  };
+
+  // items, input field, add item
   const [items, setItems] = useState<Item[]>([
     { name: "milk", price: "2", id: uuid() },
   ]);
+
   const [inputItem, setInputItem] = useState<Item>({
     name: "",
     price: "",
     id: "",
   });
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = e.target;
 
     setInputItem((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-
+  const handleAdd = (): void => {
     inputItem.id = uuid();
 
     setItems((prev) => [...prev, inputItem]);
     console.log(items);
   };
 
+  // edit
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [editId, setEditId] = useState<string>("");
   const handleIsEditing = (editId: string): void => {
@@ -46,12 +56,18 @@ function App() {
     setEditId(editId);
   };
 
-  const handleEditSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
+  const handleEdit = (): void => {
     inputItem.id = editId;
 
     setItems((prev) => {
       return prev.map((item) => (item.id === editId ? { ...inputItem } : item));
+    });
+  };
+
+  // delete
+  const handleDelete = (id: string): void => {
+    setItems((prev) => {
+      return prev.filter((item) => item.id !== id);
     });
   };
 
@@ -74,9 +90,13 @@ function App() {
             <button type="button" onClick={changeBudget}>
               Change budget
             </button>
+
             <p>$ {budget}</p>
           </>
         )}
+        <button type="button" onClick={handleReset}>
+          Reset
+        </button>
       </div>
 
       <div>
@@ -87,25 +107,30 @@ function App() {
               style={{ backgroundColor: "pink", marginBottom: "1rem" }}
             >
               {isEditing && item.id === editId ? (
-                <form onSubmit={(e) => handleEditSubmit(e)}>
+                <>
                   <input
                     type="text"
+                    name="name"
                     defaultValue={item.name}
                     onChange={(e) => handleChange(e)}
                   />
                   <input
                     type="text"
+                    name="price"
                     defaultValue={item.price}
                     onChange={(e) => handleChange(e)}
                   />
                   <button
                     type="submit"
-                    onClick={() => handleIsEditing(item.id)}
+                    onClick={() => {
+                      handleIsEditing(item.id);
+                      handleEdit();
+                    }}
                   >
                     {" "}
                     Done{" "}
                   </button>
-                </form>
+                </>
               ) : (
                 <>
                   <p>{item.name}</p>
@@ -119,30 +144,32 @@ function App() {
                   </button>
                 </>
               )}
-              <button type="button">Delete</button>
+              <button type="button" onClick={() => handleDelete(item.id)}>
+                Delete
+              </button>
             </div>
           ))}
       </div>
 
       <div>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Apple"
-            required
-            onChange={(e) => handleChange(e)}
-          />
-          ${" "}
-          <input
-            type="text"
-            name="price"
-            placeholder="1.05"
-            required
-            onChange={(e) => handleChange(e)}
-          />
-          <button type="button">Add</button>
-        </form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Apple"
+          required
+          onChange={(e) => handleChange(e)}
+        />
+        ${" "}
+        <input
+          type="text"
+          name="price"
+          placeholder="1.05"
+          required
+          onChange={(e) => handleChange(e)}
+        />
+        <button type="button" onClick={handleAdd}>
+          Add
+        </button>
       </div>
     </div>
   );
