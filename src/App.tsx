6750f3fa611ad3a948370761e7de2ae7781f25bc
MahoMori <tr27e1104@gmail.com>
2022-03-02 20:26:46 -0800

    added toFloat function

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,28 @@ import { v4 as uuid } from "uuid";
 
 type Item = {
   name: string;
-  price: string;
+  beforeD: string;
+  afterD: string;
   id: string;
 };
 
-// 数字かどうかチェック
+// type Item = {
+//   name: string;
+//   price: string;
+//   id: string;
+// };
+
+// type Price = {
+//   beforeD: string;
+//   afterD: string;
+// }
+
+// editで変わってなかったら何もしない
+// deleteで値段を戻す
 
 function App() {
   // budget
-  const [budget, setBudget] = useState<string>("0");
+  const [budget, setBudget] = useState<string>("0.00");
   const [isChangingBudget, setIsChangingBudget] = useState<boolean>(false);
   const changeBudget = (): void => {
     isChangingBudget ? setIsChangingBudget(false) : setIsChangingBudget(true);
@@ -20,18 +33,55 @@ function App() {
   // reset
   const handleReset = (): void => {
     if (window.confirm("Do you want to reset all?")) {
+      setBudget("0.00");
       setItems([]);
     }
   };
 
+  // price before decimal and after decimal
+  // const [priceState, setPriceState] = useState<Price>({beforeD: "", afterD: ""})
+  const checkNum = (): boolean => {
+    const { beforeD, afterD } = inputItem;
+    if (
+      beforeD.match(/^[0-9]*$/) !== null &&
+      afterD.match(/^[0-9]*$/) !== null
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const priceCheck = (): void => {
+    const { beforeD, afterD } = inputItem;
+    const inputTotal: number = parseFloat(`${beforeD}.${afterD}`);
+    let totalBudget: number = parseFloat(budget);
+    totalBudget -= inputTotal;
+
+    setBudget(totalBudget.toString());
+
+    if (totalBudget < 0) {
+      console.log("below budget");
+    }
+  };
+
+  const toFloat = (str: string): number => {
+    return parseFloat(str);
+  };
+
   // items, input field, add item
   const [items, setItems] = useState<Item[]>([
-    { name: "milk", price: "2", id: uuid() },
+    { name: "milk", beforeD: "1", afterD: "00", id: "707565769708" },
   ]);
+
+  // const [items, setItems] = useState<Item[]>([
+  //   { name: "milk", price: "2", id: uuid() },
+  // ]);
 
   const [inputItem, setInputItem] = useState<Item>({
     name: "",
-    price: "",
+    beforeD: "",
+    afterD: "",
     id: "",
   });
 
@@ -42,32 +92,54 @@ function App() {
   };
 
   const handleAdd = (): void => {
-    inputItem.id = uuid();
+    if (checkNum()) {
+      inputItem.id = uuid();
+      priceCheck();
 
-    setItems((prev) => [...prev, inputItem]);
-    console.log(items);
+      setItems((prev) => [...prev, inputItem]);
+      setInputItem({ name: "", beforeD: "", afterD: "", id: "" });
+    } else {
+      alert("Not a number.");
+    }
   };
 
   // edit
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [editId, setEditId] = useState<string>("");
-  const handleIsEditing = (editId: string): void => {
+
+  const handleIsEditing = (editItem: Item): void => {
     isEditing ? setIsEditing(false) : setIsEditing(true);
-    setEditId(editId);
+    setEditId(editItem.id);
+    setInputItem({ ...editItem });
   };
 
   const handleEdit = (): void => {
-    inputItem.id = editId;
+    if (checkNum()) {
+      inputItem.id = editId;
+      priceCheck();
 
-    setItems((prev) => {
-      return prev.map((item) => (item.id === editId ? { ...inputItem } : item));
-    });
+      setItems((prev) => {
+        return prev.map((item) =>
+          item.id === editId ? { ...inputItem } : item
+        );
+      });
+
+      setInputItem({ name: "", beforeD: "", afterD: "", id: "" });
+      setEditId("");
+    } else {
+      alert("Not a number.");
+    }
   };
 
   // delete
-  const handleDelete = (id: string): void => {
+  const handleDelete = (delItem: Item): void => {
+    const inputTotal: number = toFloat(`${delItem.beforeD}.${delItem.afterD}`);
+    let totalBudget: number = toFloat(budget);
+    totalBudget += inputTotal;
+    setBudget(totalBudget.toString());
+
     setItems((prev) => {
-      return prev.filter((item) => item.id !== id);
+      return prev.filter((item) => item.id !== delItem.id);
     });
   };
 
@@ -80,6 +152,7 @@ function App() {
             <input
               type="text"
               value={budget}
+              inputMode="numeric"
               onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
                 setBudget(e.target.value)
               }
@@ -100,6 +173,37 @@ function App() {
       </div>
 
       <div>
+        <input
+          type="text"
+          name="name"
+          placeholder="Apple"
+          required
+          onChange={(e) => handleChange(e)}
+        />
+        <div>
+          ${" "}
+          <input
+            type="text"
+            name="beforeD"
+            placeholder="1"
+            inputMode="numeric"
+            onChange={(e) => handleChange(e)}
+          />
+          <span>.</span>
+          <input
+            type="text"
+            name="afterD"
+            placeholder="00"
+            inputMode="numeric"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <button type="button" onClick={handleAdd}>
+          Add
+        </button>
+      </div>
+
+      <div>
         {items.length > 0 &&
           items.map((item) => (
             <div
@@ -112,21 +216,25 @@ function App() {
                     type="text"
                     name="name"
                     defaultValue={item.name}
+                    required
                     onChange={(e) => handleChange(e)}
                   />
                   <input
                     type="text"
-                    name="price"
-                    defaultValue={item.price}
+                    name="beforeD"
+                    defaultValue={item.beforeD}
+                    inputMode="numeric"
                     onChange={(e) => handleChange(e)}
                   />
-                  <button
-                    type="submit"
-                    onClick={() => {
-                      handleIsEditing(item.id);
-                      handleEdit();
-                    }}
-                  >
+                  <span>.</span>
+                  <input
+                    type="text"
+                    name="afterD"
+                    defaultValue={item.afterD}
+                    inputMode="numeric"
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <button type="button" onClick={() => handleEdit()}>
                     {" "}
                     Done{" "}
                   </button>
@@ -134,42 +242,20 @@ function App() {
               ) : (
                 <>
                   <p>{item.name}</p>
-                  <p>$ {item.price}</p>
-                  <button
-                    type="button"
-                    onClick={() => handleIsEditing(item.id)}
-                  >
+                  <p>
+                    $ {item.beforeD}.{item.afterD}
+                  </p>
+                  <button type="button" onClick={() => handleIsEditing(item)}>
                     {" "}
                     Edit{" "}
                   </button>
                 </>
               )}
-              <button type="button" onClick={() => handleDelete(item.id)}>
+              <button type="button" onClick={() => handleDelete(item)}>
                 Delete
               </button>
             </div>
           ))}
-      </div>
-
-      <div>
-        <input
-          type="text"
-          name="name"
-          placeholder="Apple"
-          required
-          onChange={(e) => handleChange(e)}
-        />
-        ${" "}
-        <input
-          type="text"
-          name="price"
-          placeholder="1.05"
-          required
-          onChange={(e) => handleChange(e)}
-        />
-        <button type="button" onClick={handleAdd}>
-          Add
-        </button>
       </div>
     </div>
   );
